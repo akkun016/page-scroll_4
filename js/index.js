37fa5c120222110d8c05fdacff79ded354cfa1b6
akkun016 <nakataakito@icloud.com>
2023-04-20 18:24:21 +0900

    index.js_fix1

--- js/index.js
+++ js/index.js
@@ -3,24 +3,41 @@ $(function () {
   const nav = $(".header-nav__lists").find("a");
 
   let total = 0;
-  let totalHeightArr = [];
+  let totalBottomLineArr = [];
   for (let i = 0; i < section.length; i++) {
     const block = section.eq(i);
 
     total += block.outerHeight();
-    totalHeightArr.push(total);
+    totalBottomLineArr.push(total);
   }
 
+
+  nav.click(function (e) {
+    e.preventDefault();
+
+    const href = $(this).attr("href").replace('#', '');
+    const target = $("#" + href);
+    const targetHight = target.offset().top;
+
+    window.scrollTo({
+      top: targetHight,
+      behavior: 'smooth'
+    });
+  })
+
+
   $(window).scroll(function () {
-    let screenTop = $(this).scrollTop();
+    window.setTimeout(function () {
+      const screenTop = $(this).scrollTop();
     
-    for (let int = 0; int < section.length; int++) {
-      nav.eq(int).removeClass("active");
-      if (int === 0 && screenTop < totalHeightArr[int]) {
-        nav.eq(int).addClass("active");
-      } else if (totalHeightArr[int- 1] <= screenTop && screenTop < totalHeightArr[int]) {
-        nav.eq(int).addClass("active");
+      for (let int = 0; int < section.length; int++) {
+        nav.eq(int).removeClass("active");
+        if (int === 0 && screenTop < totalBottomLineArr[int]) {
+          nav.eq(int).addClass("active");
+        } else if (totalBottomLineArr[int- 1] <= screenTop && screenTop < totalBottomLineArr[int]) {
+          nav.eq(int).addClass("active");
+        }
       }
-    }
+    }, 700);
   })
 });
